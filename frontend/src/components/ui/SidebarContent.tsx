1c4f75add500fd1a04a4dd3095cd30968dcac994
maiei <146412593+marie-criz@users.noreply.github.com>
2025-06-28 10:20:30 +0800

    chore(sidebar): removed Area Status

--- frontend/src/components/ui/SidebarContent.tsx
+++ frontend/src/components/ui/SidebarContent.tsx
@@ -1,7 +1,4 @@
 import React from 'react';
-import flood from '../../assets/flood.svg';
-import droplet from '../../assets/dropley.svg';
-import SidebarNearbySupport from "./SidebarNearbySupport.tsx";
 
 interface SidebarContentProps {
     currentCoords: {
@@ -50,42 +47,9 @@ export const SidebarContent: React.FC<SidebarContentProps> = ({
 
     return (
         <div className=" p-6 pb-5">
-            {/* Status Section */}
-            <div className="mb-8">
-                <h2 className="text-xl font-bold text-[#066AAA] text-center mb-5">
-                    Your Area's Status
-                </h2>
-                <div className="grid grid-cols-2 gap-4 mb-8">
-                    <div className="bg-white border border-slate-200 rounded-xl p-5 text-center shadow-sm">
-                        <div className="text-xs text-slate-500 font-medium mb-4 uppercase tracking-wide">
-                            Water Level
-                        </div>
-                        <div className="flex justify-center mb-4">
-                            <img src={droplet} alt="droplet" className="w-20" />
-                        </div>
-                        <div className="text-xs font-semibold px-3 py-1.5 rounded-full bg-green-100 text-green-800 uppercase tracking-wide">
-                            Normal
-                        </div>
-                    </div>
-                    <div className="bg-white border border-slate-200 rounded-xl p-5 text-center shadow-sm">
-                        <div className="text-xs text-slate-500 font-medium mb-4 uppercase tracking-wide">
-                            Flood Hazard
-                        </div>
-                        <div className="flex justify-center mb-4">
-
-                        <img src={flood} alt="flood" className="w-20" />
-                            </div>
-                        <div className="text-xs font-semibold px-3 py-1.5 rounded-full bg-green-100 text-green-800 uppercase tracking-wide">
-                            No Flooding
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <SidebarNearbySupport></SidebarNearbySupport>
 
             {/* Map Controls Section */}
-            <div className="mt-6 pt-5 border-t border-slate-200">
+            <div className="pt-5 border-t border-slate-200">
                 <div
                     className="text-base font-semibold text-slate-800 mb-4 flex items-center cursor-pointer select-none"
                     onClick={toggleMapControls}
